aed4d7b8ff914dd2223114067e85809b639d2a6e
Paulo Braz <paulo_braz@unifio.edu.br>
2022-09-26 22:00:31 -0300

    Adicionando funções ao JS

--- AnaliseSentimentos4/Front/script.js
+++ AnaliseSentimentos4/Front/script.js
@@ -10,3 +10,24 @@ function lightMode() {
     element.className = "Modo Claro";
     content.innerText = "OFF";
 }
+
+function Get(url){
+    let request = new XMLHttpRequest(); // a new request
+    request.open("GET",url,false);
+    request.send();
+    return request.responseText;          
+}
+function criaLinha(Usuario){
+
+
+}
+
+function main(){
+    var url = "http://localhost:5000/api/Usuario";
+    var data = Get(url);
+    var Usuario = JSON.parse(data);
+    console.log(Usuario);
+    criaLinha(Usuario);
+}
+
+main();
