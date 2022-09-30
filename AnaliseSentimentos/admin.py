b395f76741a66b07d020a844dec34e940f046ba8
Paulo Braz <paulo_braz@unifio.edu.br>
2022-09-29 21:22:10 -0300

    Alterações banco de dados

--- AnaliseSentimentos/admin.py
+++ AnaliseSentimentos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from AnaliseSentimentos.models import Login, Registro
+
 # Register your models here.
+admin.site.register(Registro)
+admin.site.register(Login)
--- AnaliseSentimentos/models.py
+++ AnaliseSentimentos/models.py
@@ -4,13 +4,19 @@ from django.db import models
 
 
 class Registro(models.Model):
-    nome = models.CharField(max_length=150, verbose_name='Nome')
-    email = models.CharField(max_length=100, verbose_name='Email')
+    nome = models.CharField(max_length=150,)
+    email = models.EmailField(max_length=100, verbose_name='Email')
     senha = models.CharField(max_length=150, verbose_name='Senha')
 
     def __str__(self):
         return "{} ({}) ({})".format(self.nome, self.email, self.senha)
 
-# class Login(models.Model):
-#     email = models.CharField(max_length=100)
-#     senha = models.CharField(max_length=150)
+
+class Login(models.Model):
+     email = models.EmailField(max_length=100, verbose_name='Email')
+     senha = models.CharField(max_length=150)
+     
+     registro = models.ForeignKey(Registro, on_delete=models.PROTECT)
+    
+     def __str__(self):
+        return " {} ({})".format(self.nome, self.registro.nome)
