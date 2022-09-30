03f1cd8cd747e51a5a98ebf7492b662594cab66a
Paulo Braz <paulo_braz@unifio.edu.br>
2022-09-30 02:25:26 -0300

    ...

--- AnaliseSentimentos/models.py
+++ AnaliseSentimentos/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Registro(models.Model):
     nome = models.CharField(max_length=150,)
-    email = models.EmailField(max_length=100, verbose_name='Email')
+    email = models.CharField(max_length=100, verbose_name='Email')
     senha = models.CharField(max_length=150, verbose_name='Senha')
 
     def __str__(self):
@@ -13,10 +13,19 @@ class Registro(models.Model):
 
 
 class Login(models.Model):
-     email = models.EmailField(max_length=100, verbose_name='Email')
-     senha = models.CharField(max_length=150)
-     
-     registro = models.ForeignKey(Registro, on_delete=models.PROTECT)
-    
-     def __str__(self):
+    email = models.EmailField(max_length=100, verbose_name='Email')
+    senha = models.CharField(max_length=150)
+
+    registro = models.ForeignKey(Registro, on_delete=models.PROTECT)
+
+    def __str__(self):
         return " ({})".format(self.registro.nome)
+
+
+class Letra(models.Model):
+    letra = models.TextField(max_length=150, verbose_name='Letra')
+
+    login = models.ForeignKey(Login, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return " ({})".format(self.login.registro.nome)
--- AnaliseSentimentos/views.py
+++ AnaliseSentimentos/views.py
@@ -1,35 +1,59 @@
-from django.shortcuts import render, redirect
-from AnaliseSentimentos.models import Registro, Login
-from AnaliseSentimentos.registro import RegistroForm
+# from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from AnaliseSentimentos.models import Letra, Registro, Login
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 
 
-def home(request):
-    data = {}
-    data['db'] = Registro.objects.all()
-
-    return render(request, 'index.html', data)
-
-
-def registro(request):
-    data = {}
-    data['registro'] = RegistroForm()
-    return render(request, 'registro.html', data)
+class LoginCreate(CreateView):
+    model = Login
+    fields = ['email',
+              'senha', ]
+    template_name: str = 'index.html'
+    success_url: reverse_lazy('letra')
 
 
-def novoUsuario(request):
-    form = RegistroForm(request.POST or None)
-    if form.is_valid():
-          form.save()
-          return redirect('home')
+class RegistroCreate(CreateView):
+    model = Registro
+    fields = ['nome',
+              'email',
+              'senha', ]
+    template_name: str = 'registro.html'
+    success_url: reverse_lazy('index')
 
 
-def view(request, pk):
-    data = {}
-    data['db'] = Registro.objects.get(pk=pk)
-    return render(request, 'view.html', data)
- 
+class LetraCreate(CreateView):
+    model = Letra
+    fields = ['letra', ]
+    template_name: str = 'letra.html'
+    success_url: reverse_lazy('minhasLetras')
+
+# def home(request):
+#     data = {}
+#     data['db'] = Registro.objects.all()
+
+#     return render(request, 'index.html', data)
+
+
+# def registro(request):
+#     data = {}
+#     data['registro'] = RegistroForm()
+#     return render(request, 'registro.html', data)
+
+
+# def novoUsuario(request):
+#     form = RegistroForm(request.POST or None)
+#     if form.is_valid():
+#           form.save()
+#           return redirect('home')
+
+
+# def view(request, pk):
+#     data = {}
+#     data['db'] = Registro.objects.get(pk=pk)
+#     return render(request, 'view.html', data)
+
 
 # def letra(request):
 #     data = {}
